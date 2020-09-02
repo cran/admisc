ec8a9e5c5fffc37e6cf866b42842f0d64a6ca867
Adrian Dusa <dusa.adrian@unibuc.ro>
2020-09-02 06:20:05 +0000

    version 0.9

--- R/translate.R
+++ R/translate.R
@@ -35,7 +35,7 @@ function(expression = "", snames = "", noflevels = NULL, data = NULL, ...) {
     }
     if (any(grepl("<=>|<->|=>|->|<=|<-", expression))) {
         cat(enter)
-        stop(simpleError(paste0("Incorrect expression.", enter, enter)))
+        stop(simpleError(paste0("Incorrect expression, contains outcome and relation.", enter, enter)))
     }
     if (!is.vector(snames)) {
         cat(enter)
@@ -225,7 +225,7 @@ function(expression = "", snames = "", noflevels = NULL, data = NULL, ...) {
         }
     }
     else {
-        sl <- ifelse(replaced | identical(snames, ""), TRUE, all(nchar(snames) == 1))
+        sl <- ifelse((replaced & length(snames) < 27) | identical(snames, ""), TRUE, all(nchar(snames) == 1))
         pp <- unlist(strsplit(expression, split = "[+]"))
         if (replaced) {
             pp <- gsub("[*]", "", pp)
